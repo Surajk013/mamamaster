75b0480cf90a10737717581f3af439f6dbec6274
IamUsike <sathwikthanmay4@gmail.com>
2025-05-27 20:33:15 +0530

    login

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,6 +1,5 @@
 import { User } from "../models/user.model.js";
 import jwt from "jsonwebtoken";
-import mongoose from "mongoose";
 
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
@@ -8,6 +7,23 @@ import { uploadOnCloudinary } from "../utils/cloudinary.js";
 //save it locally temporarily  and then upload it to cloudinary
 //remove the local image.
 // Send the url of the image on cloudinary to the user
+
+export const generateAccessToken = async (userId, res) => {
+  try {
+    const user = await User.findById(userId);
+    //for now since we calling this from login function (we've already verified
+    //if the user exists there itself). If calling from other places check verification.
+
+    const accessToken = user.generateAccessToken();
+
+    return accessToken;
+  } catch (error) {
+    console.log("Error while generating access token", error.message);
+    return res
+      .status(500)
+      .json({ message: "Something went wrong while logging in" });
+  }
+};
 
 export const registerUser = async (req, res) => {
   try {
@@ -60,5 +76,48 @@ export const registerUser = async (req, res) => {
     return res
       .status(500)
       .json({ message: "Something went wrong while registering the user" });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password)
+      return res
+        .status(400)
+        .json({ message: "All the fields should be filled" });
+
+    const user = await User.findOne({
+      email,
+    });
+
+    if (!user)
+      return res
+        .status(404)
+        .json({ message: "User with provided email doesnt exist" });
+
+    const isPasswordValid = await user.isPasswordCorrect(password);
+    if (!isPasswordValid)
+      return res.status(401).json({ message: "Password Invalid" });
+
+    const accessToken = await generateAccessToken(user._id);
+
+    const loggedInUser = await User.findById(user._id).select("-password");
+
+    const options = {
+      httpOnly: true,
+      secure: true,
+    };
+
+    return res
+      .status(200)
+      .cookie("accessToken", accessToken, options)
+      .json({ messsage: "User logged in successfully", user: loggedInUser });
+  } catch (error) {
+    console.log("Error logging in user", error.message);
+    return res
+      .status(500)
+      .json({ message: "Something went wrong while logging in" });
   }
 };
